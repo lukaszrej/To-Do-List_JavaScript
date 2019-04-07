cf2e2d1c72708c9c7eae969dfbd6547fd51ee7dc
Lukasz Rej <rejlukasz@gmail.com>
2019-04-07 13:07:02 +0200

    Cleaning before find- and sort-functions

--- js/app.js
+++ js/app.js
@@ -1,237 +1,237 @@
-// f u n c t i o n s
+document.addEventListener("DOMContentLoaded", function () {
 
-// Validate input
-function validateInput(inputValue) {
-    if (inputValue.value === '') {
-        alert('Write something!');
-        return false;
-    } else if (inputValue.value.length <5 || inputValue.value.length >100) {
-        alert('Be specific, use between 5 to 100 characters to describe your task!');
-        return false;
-    }
+    // v a r i a b l e s
+    var taskInput = document.getElementById('taskInput');
 
-    return true;
-};
-
-// Count to-dos
-var updateCounter = function () {
-    var finishedTasksCounter = 0;
-    var counter = document.getElementsByClassName('to-do').length;
-    var counterElement = document.getElementsByClassName('to-do');
-
-    for (var i=0; i<counter; i++) {
-        var counterClasses = counterElement[i].classList;
-        for (var j=0; j<counterClasses.length; j++) {
-            if (counterClasses[j] === "done" ) {
-                finishedTasksCounter++;
-            }
-        }
-    }
-
-    counter = counter - finishedTasksCounter;
+    var taskList = document.getElementById('taskList');
+    var addTaskButton = document.getElementById('addTaskButton');
 
     var counterDisplay = document.getElementById('toFinish');
-    counterDisplay.innerHTML = counter;
-};
 
-// Edit task
-var editTask = function () {
     var editButtons = document.getElementsByClassName('edit');
-
-    for (var i=0; i<editButtons.length; i++) {
-        editButtons[i].onclick = function () {
-
-            // non-editable edit-button
-            this.setAttribute('contenteditable', 'false');
-
-            // editable / non-editable task
-            var div = this.parentElement;
-
-            // allow / don't allow to edit task
-            if (this.firstChild.classList.value === "fas fa-edit") {
-                // change the icons
-                this.firstChild.classList.remove("fa-edit");
-                this.firstChild.classList.add("fa-save");
-                // make the task editable
-                div.setAttribute('contenteditable', 'true');
-            } else if (this.firstChild.classList.value === "fas fa-save") {
-                // change the icons
-                this.firstChild.classList.remove("fa-save");
-                this.firstChild.classList.add("fa-edit");
-                // make the task non-editable
-                div.setAttribute('contenteditable', 'false');
-            }
-
-        };
-    }
-};
-
-// Mark as completed
-var markAsCompleted = function() {
     var completeButton = document.getElementsByClassName('complete');
-
-    for (var i=0; i < completeButton.length; i++) {
-        completeButton[i].onclick = function () {
-            var divText = this.parentElement.parentElement.querySelector('h2');
-
-            var editIcon = this.parentElement.previousElementSibling.querySelector('.edit i');
-            divText.classList.toggle('done');
-            var divTask = divText.parentElement.parentElement;
-            if (divTask.firstElementChild.hasAttribute('contenteditable')) {
-                divTask.firstElementChild.setAttribute('contenteditable', 'false');
-                editIcon.classList.remove('fa-save');
-                editIcon.classList.add('fa-edit')
-            }
-
-            // if item is marked as completed remove the edit button
-            var editButtonToRemove = divText.nextElementSibling;
-            editButtonToRemove.classList.toggle('none');
-
-            updateCounter();
-        }
-    }
-};
-
-// Delete an item from the list
-var deleteFromList = function() {
     var deleteButton = document.getElementsByClassName('close');
 
-    for (var i = 0; i < deleteButton.length; i++) {
-        deleteButton[i].onclick = function() {
-            var div = this.parentElement.parentElement;
-            div.remove();
-
-            updateCounter();
-        }
-    }
-};
-
-// Delete all completed items
-var deleteAllItems = function() {
     var removeAll = document.getElementById('removeFinishedTasksButton');
-    var taskList = document.getElementById('taskList');
 
-    removeAll.addEventListener('click', function () {
-        var lis = taskList.querySelectorAll('.done');
-
-        for (var i=0; i<lis.length; i++) {
-            lis[i].parentElement.parentElement.remove();
-        }
-
-        updateCounter();
-    });
-
-};
-
-// Find task
-var findTask = function() {
     var searchButton = document.querySelector('.searchButton');
     var foundedValue = "";
 
-    searchButton.addEventListener('click', function(e){
-        var listElements = document.querySelector("#taskList");
-        var li = listElements.querySelectorAll('li');
-        var inputSearchElement = document.querySelector('#search');
-        var inputSearchValue = inputSearchElement.value;
-        var ul = document.querySelector('ul');
 
-        for(var i=0; i<li.length; i++){
+    // f u n c t i o n s
 
-            var taskText = li[i].querySelector('.to-do').innerHTML.toLowerCase();
-
-                if(taskText === inputSearchValue.toLowerCase()){
-                    foundedValue = li[i];
-                    this.innerHTML = "Show all";
-                } else {
-                    li[i].remove();
-                }
+    // Validate input
+    var validateInput = function(inputValue) {
+        if (inputValue.value === '') {
+            alert('Write something!');
+            return false;
+        } else if (inputValue.value.length <5 || inputValue.value.length >100) {
+            alert('Be specific, use between 5 to 100 characters to describe your task!');
+            return false;
         }
 
-        ul.appendChild(foundedValue);
-        updateCounter();
+        return true;
+    };
 
-    });
-};
+    // Count to-dos
+    var updateCounter = function () {
+        var finishedTasksCounter = 0;
+        var counter = document.getElementsByClassName('to-do').length;
+        var counterElement = document.getElementsByClassName('to-do');
 
-// Sort tasks
-var sortTasks = function() {
-    var lis = document.querySelectorAll('.item-container');
-    var ul = document.querySelector('#taskList');
-    var sortButton = document.querySelector('#sort');
-
-    var oldLis = [];
-    for (var i=0; i<lis.length; i++) {
-        oldLis.push(lis[i]);
-
-    }
-    console.log(lis);
-
-
-    var labels = [];
-
-    for (var i=0; i<lis.length; i++) {
-        // oldLis.push(lis[i]);
-
-        var currentLis = lis[i].querySelector('h2').innerText.toLowerCase();
-        labels.push(currentLis);
-
-        var sortedLabels = labels.sort();
-    }
-
-    // console.log(oldLis);
-
-
-
-
-
-    if (sortButton.innerText === 'SORT') {
-
-
-            for (var i=0; i<sortedLabels.length; i++) {
-                for (var j=0; j<lis.length; j++) {
-                    if (sortedLabels[i] === lis[j].querySelector('h2').innerText.toLowerCase()) {
-                        ul.appendChild(lis[j]);
-                    }
+        for (var i=0; i<counter; i++) {
+            var counterClasses = counterElement[i].classList;
+            for (var j=0; j<counterClasses.length; j++) {
+                if (counterClasses[j] === "done" ) {
+                    finishedTasksCounter++;
                 }
-                // sortButton.innerHTML = "UNSORT";
+            }
+        }
 
+        counter = counter - finishedTasksCounter;
+
+        counterDisplay.innerHTML = counter;
+    };
+
+    // Edit task
+    var editTask = function () {
+        for (var i=0; i<editButtons.length; i++) {
+            editButtons[i].onclick = function () {
+
+                // non-editable edit-button
+                this.setAttribute('contenteditable', 'false');
+
+                // editable / non-editable task
+                var div = this.parentElement;
+
+                // allow / don't allow to edit task
+                if (this.firstChild.classList.value === "fas fa-edit") {
+                    // change the icons
+                    this.firstChild.classList.remove("fa-edit");
+                    this.firstChild.classList.add("fa-save");
+                    // make the task editable
+                    div.setAttribute('contenteditable', 'true');
+                } else if (this.firstChild.classList.value === "fas fa-save") {
+                    // change the icons
+                    this.firstChild.classList.remove("fa-save");
+                    this.firstChild.classList.add("fa-edit");
+                    // make the task non-editable
+                    div.setAttribute('contenteditable', 'false');
+                }
+
+            };
+        }
+    };
+
+    // Mark as completed
+    var markAsCompleted = function() {
+        for (var i=0; i < completeButton.length; i++) {
+            completeButton[i].onclick = function () {
+                var divText = this.parentElement.parentElement.querySelector('h2');
+
+                var editIcon = this.parentElement.previousElementSibling.querySelector('.edit i');
+                divText.classList.toggle('done');
+                var divTask = divText.parentElement.parentElement;
+                if (divTask.firstElementChild.hasAttribute('contenteditable')) {
+                    divTask.firstElementChild.setAttribute('contenteditable', 'false');
+                    editIcon.classList.remove('fa-save');
+                    editIcon.classList.add('fa-edit');
+                }
+
+                // if item is marked as completed remove the edit button
+                var editButtonToRemove = divText.nextElementSibling;
+                editButtonToRemove.classList.toggle('none');
+
+                updateCounter();
+            }
+        }
+    };
+
+    // Delete an item from the list
+    var deleteFromList = function() {
+        for (var i = 0; i < deleteButton.length; i++) {
+            deleteButton[i].onclick = function() {
+                var div = this.parentElement.parentElement;
+                div.remove();
+
+                updateCounter();
+            }
+        }
+    };
+
+    // Delete all completed items
+    var deleteAllItems = function() {
+        removeAll.addEventListener('click', function () {
+            var completedLis = taskList.querySelectorAll('.done');
+
+            for (var i=0; i<completedLis.length; i++) {
+                completedLis[i].parentElement.parentElement.remove();
             }
 
-            sortButton.innerText = "UNSORT";
+            updateCounter();
+        });
 
+    };
 
-        } else if (sortButton.innerText === 'UNSORT') {
+    // Find task
+    var findTask = function() {
+        searchButton.addEventListener('click', function(e){
+            e.preventDefault();
 
-        console.log('ej ej')
+            var li = taskList.querySelectorAll('li');
+            var inputSearchElement = document.querySelector('#search');
+            var inputSearchValue = inputSearchElement.value;
 
-            for (var i=0; i<lis.length; i++) {
+            for(var i=0; i<li.length; i++){
 
-                oldLis.push(lis[i]);
-                // ul.appendChild(oldLis[i]);
+                var taskText = li[i].querySelector('.to-do').innerHTML.toLowerCase();
+
+                    if(taskText === inputSearchValue.toLowerCase()){
+                        foundedValue = li[i];
+                        this.innerHTML = "Show all";
+                    }
             }
 
-            sortButton.innerText = 'SORT';
+            taskList.appendChild(foundedValue);
+            updateCounter();
 
-    }
-
-};
-
+        });
 
 
-// m a i n
+    };
 
-document.addEventListener("DOMContentLoaded", function () {
-    var lis = document.querySelectorAll('li');
-    var taskList = document.getElementById('taskList');
-    var addTaskButton = document.getElementById('addTaskButton');
+    // Sort tasks
+    var sortTasks = function() {
+        var lis = document.querySelectorAll('.item-container');
+        var ul = document.querySelector('#taskList');
+        var sortButton = document.querySelector('#sort');
+
+        var oldLis = [];
+        for (var i=0; i<lis.length; i++) {
+            oldLis.push(lis[i]);
+
+        }
+        console.log(lis);
+
+
+        var labels = [];
+
+        for (var i=0; i<lis.length; i++) {
+            // oldLis.push(lis[i]);
+
+            var currentLis = lis[i].querySelector('h2').innerText.toLowerCase();
+            labels.push(currentLis);
+
+            var sortedLabels = labels.sort();
+        }
+
+        // console.log(oldLis);
+
+
+
+
+
+        if (sortButton.innerText === 'SORT') {
+
+
+                for (var i=0; i<sortedLabels.length; i++) {
+                    for (var j=0; j<lis.length; j++) {
+                        if (sortedLabels[i] === lis[j].querySelector('h2').innerText.toLowerCase()) {
+                            ul.appendChild(lis[j]);
+                        }
+                    }
+                    // sortButton.innerHTML = "UNSORT";
+
+                }
+
+                sortButton.innerText = "UNSORT";
+
+
+            } else if (sortButton.innerText === 'UNSORT') {
+
+            console.log('ej ej')
+
+                for (var i=0; i<lis.length; i++) {
+
+                    oldLis.push(lis[i]);
+                    // ul.appendChild(oldLis[i]);
+                }
+
+                sortButton.innerText = 'SORT';
+
+        }
+
+    };
+
+
+
+    // m a i n
 
 
     // Create a new list item
     addTaskButton.addEventListener('click', function () {
 
-        var inputValue = document.getElementById('taskInput');
-
-        if (validateInput(inputValue)) {
+        if (validateInput(taskInput)) {
 
             // creating two flexible divs
             var task = document.createElement('div');
@@ -242,7 +242,7 @@ document.addEventListener("DOMContentLoaded", function () {
             // creating text for the task (as taskTitle)
             var taskTitle = document.createElement('h2');
             taskTitle.classList.add('to-do');
-            taskTitle.innerHTML = inputValue.value;
+            taskTitle.innerHTML = taskInput.value;
 
             // creating buttons
             // editButton
@@ -300,7 +300,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         }
 
-        inputValue.value = "";
+        taskInput.value = "";
 
         markAsCompleted();
         deleteFromList();
@@ -310,10 +310,12 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
 
+
     var sortButton = document.querySelector('#sort');
     sortButton.addEventListener('click', function () {
         sortTasks();
     });
+
 
     findTask();
     deleteAllItems();
